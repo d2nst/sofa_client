9f139082296f86550f34a3dd2d8f93916f8a8c51
everydayHo <jungwoopage@gmail.com>
2024-07-23 19:55:29 +0900

    prettier, eslint 설정

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,16 +6,24 @@ module.exports = {
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
+  plugins: ['react', 'react-hooks', 'jsx-a11y', 'react-refresh'],
   rules: {
     'react/jsx-no-target-blank': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-uses-react': 'off',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'off',
+    'react/prop-types': 'off',
+    'prettier/prettier': 'error',
+    quotes: ['error', 'single', { allowTemplateLiterals: true }],
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
   },
-}
+};
