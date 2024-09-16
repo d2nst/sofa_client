c5ed4ae7ae4efe52cc0dde46b151d144d39c1494
d2nst <wlsdl950@gmail.com>
2024-09-16 15:59:50 +0900

    사이즈 변경

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/navbar.css';
 
@@ -6,22 +6,29 @@ import Logo from '../assets/sofa.png';
 import MenuIcon from '../assets/menu.png';
 
 function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <div className="navbar">
+      <div className="leftSide"></div>
       <div className="middleSide">
         <Link to="/">
           <img src={Logo} style={{ width: '124px', height: '40px' }} />
         </Link>
       </div>
       <div className="rightSide">
-        <img src={MenuIcon} style={{ width: '24px', height: '24px' }} />
+        <img
+          src={MenuIcon}
+          style={{ width: '24px', height: '24px' }}
+          onClick={() => setMenuOpen(!menuOpen)}
+        />
+        {menuOpen && (
+          <ul className="menu dropdown">
+            <li>menu1</li>
+            <li>menu2</li>
+          </ul>
+        )}
       </div>
-      <ul>
-        <li>
-          <Link to="/">로그인</Link>
-        </li>
-        <li>마이페이지</li>
-      </ul>
     </div>
   );
 }
