6d2cf69acb53d2fbe95aca2a49d62209dbec9d06
everydayHo <jungwoopage@gmail.com>
2024-09-16 12:34:23 +0900

    컴포넌트변경

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@ const Button = () => {
   return (
     <div>
       <a href="">
-        <img src="public/images/kakao_login_btn.png" alt="카카오 로그인" />
+        <img src="/images/kakao_login_btn.png" alt="카카오 로그인" />
       </a>
     </div>
   );
