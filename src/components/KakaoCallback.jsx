ba0bf89d466e8b1f5f5107456731b33cfa0f5deb
everydayHo <jungwoopage@gmail.com>
2024-09-18 14:17:24 +0900

    인증후 리다이렉트 경로수정

--- src/components/KakaoCallback.jsx
+++ src/components/KakaoCallback.jsx
@@ -1,11 +1,11 @@
 import React, { useEffect, useState } from 'react';
-// import { useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 
 const KakaoCallback = () => {
   const KAKAO_CODE = new URL(window.location.href).searchParams.get('code');
   const [accessTokenFetching, setAccessTokenFetching] = useState(false);
-  // const navigate = useNavigate();
+  const navigate = useNavigate();
   // Access Token 받아오기
   const getAccessToken = async () => {
     if (accessTokenFetching) return; // Return early if fetching
@@ -34,7 +34,7 @@ const KakaoCallback = () => {
       // });
 
       setAccessTokenFetching(false); // Reset fetching to false
-      // navigate('/kakaologin');
+      navigate('/kakaologin');
     } catch (error) {
       console.error('Error:', error);
       setAccessTokenFetching(false); // Reset fetching even in case of error
